b74d90944994b411eaccbaa542a7e364fdfc5ad3
webstarlee <webstar.lee@gmail.com>
2023-06-02 17:55:11 +0900

    basic function all done

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,12 +11,18 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 import { VDataTable } from 'vuetify/labs/VDataTable'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import * as labs from 'vuetify/labs/components'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   components: {
     VDataTable,
+    ...components,
+    ...labs
   },
+  directives,
   theme: {
     defaultTheme: 'dark',
   },
--- src/stores/index.js
+++ src/stores/index.js
@@ -3,3 +3,4 @@ export * from './auth.store';
 export * from './users.store';
 export * from './groups.store';
 export * from './setting.store';
+export * from './tokens.store'
